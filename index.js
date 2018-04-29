8f31a038961e4bd526e6380a2b84fba3f441f64b
Quang-Tri <qtrido@localhost.localdomain>
2018-04-29 17:03:37 -0400

    starting responsive design

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+$("#side-projects").click(function() {
+    $('html, body').animate({
+        scrollTop: ($('.side-projects').offset().top)
+    },500);
+})
+
+$("#work-experience").click(function() {
+    $('html, body').animate({
+        scrollTop: ($('.work-experience').offset().top)
+    },500);
+})
+
 $("#programming").click(function() {
-    $.scrollTo('.side-projects', 500);
+    $('html, body').animate({
+        scrollTop: ($('.programming').offset().top)
+    },500);
 })
